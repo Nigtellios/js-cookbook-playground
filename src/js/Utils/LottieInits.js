44a5d40cdb6ade36ebe282938c1254dc846d071e
Jakub Kowalski <contact@jakubkowalski.dev>
2022-03-27 13:01:13 +0200

    🥷Added: Lottie animations on home

--- src/js/Utils/LottieInits.js
+++ src/js/Utils/LottieInits.js
@@ -1,4 +1,6 @@
 import bodymovin from 'lottie-web';
+import * as moon from '../../animations/moon-phases.json';
+import * as sparkles from '../../animations/sparkles-animation.json';
 
 const lottie = {
   init(el, data) {
@@ -12,4 +14,11 @@ const lottie = {
   }
 }
 
-export default lottie;
+const animations = {
+  init() {
+    lottie.init('planet', moon);
+    lottie.init('sparkles', sparkles);
+  }
+}
+
+export { animations };
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,7 @@
 // Import Functions/Classes
 import { InfoTile } from "./Components/InfoTile";
-import lottie from "./Utils/LottieInits";
+import { animations } from "./Utils/LottieInits";
 
-// Import Animations
-import * as moon from '../animations/moon-phases.json';
-import * as sparkles from '../animations/sparkles-animation.json';
-
-// Init Lottie
-lottie.init('planet', moon);
-lottie.init('sparkles', sparkles);
+animations.init();
 
 new InfoTile('tilesScope', 'tile', {"id": "some_tile_id"});
