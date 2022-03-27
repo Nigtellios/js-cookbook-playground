cf2401b8b5c91e74ca175954c5cfe6ec1c808658
Jakub Kowalski <contact@jakubkowalski.dev>
2022-03-27 16:01:42 +0200

    🥷Added: resizeAnimation

--- src/js/Utils/LottieInits.js
+++ src/js/Utils/LottieInits.js
@@ -1,6 +1,8 @@
 import bodymovin from 'lottie-web';
+import resizeAnimation from "./ResizeAnimation";
 import * as moon from '../../animations/moon-phases.json';
 import * as sparkles from '../../animations/sparkles-animation.json';
+
 
 const lottie = {
   init(el, data) {
@@ -18,6 +20,7 @@ const animations = {
   init() {
     lottie.init('planet', moon);
     lottie.init('sparkles', sparkles);
+    resizeAnimation.init('sparkles', 3);
   }
 }
 
--- src/js/Utils/ResizeAnimation.js
+++ src/js/Utils/ResizeAnimation.js
@@ -1,11 +1,39 @@
-
+/**
+ * Resize Animation Object - Resize Selected animation to match parent width calculated based on whole window inner width
+ * @type {{init(string, string): void, calcResize(*, number): void, initResizer(number): number}}
+ */
 const resizeAnimation = {
-  init() {
-
+  /**
+   * Initialize animation resize
+   * @param { string } animationScope
+   * @param { number } scaleModifier
+   */
+  init(animationScope, scaleModifier) {
+    resizeAnimation.initResizer(scaleModifier);
+    resizeAnimation.calcResize(animationScope, scaleModifier);
+    window.addEventListener('resize', () => {
+      resizeAnimation.calcResize(animationScope, scaleModifier);
+    });
   },
 
-  resizer(animationScope) {
-    animationScope.style.transform.scale();
+  /**
+   * Init Resizer which takes selected ScaleModifier as a parameter
+   * @param { number } scaleModifier
+   * @returns {number}
+   */
+  initResizer(scaleModifier) {
+    const width = window.innerWidth;
+    return Math.round(((width / 1000) * scaleModifier) * 100) / 100;
+  },
+
+  /**
+   * Calculates proper scale value
+   * @param animationScope
+   * @param { number } scaleModifier
+   */
+  calcResize(animationScope, scaleModifier) {
+    animationScope = document.getElementById(animationScope);
+    animationScope.setAttribute('style', `transform: scale(${resizeAnimation.initResizer(scaleModifier)}) rotate(90deg);`);
   }
 }
 
